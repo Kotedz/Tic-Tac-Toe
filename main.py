58bc4123577a8209dd512e9db68ab060bbd57649
Kotedz <kotedz.is.cool@gmail.com>
2025-04-27 19:18:07 +0700

    19:18

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
+import sys, random
+from types import new_class
 
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QMessageBox
 
 class TTT(QWidget): # Tic-Tac-Toe
+
     def __init__(self):
-        super().__init__()
+        super().__init__() # init of parent
         self.init_ui()
 
     def init_ui(self):
-        pass
-
-
-
+        self.setWindowTitle("Кресты и Нули")
+        self.setGeometry(100,100,300,300) # pos and size of window
+        self.grid = QGridLayout()
+        self.setLayout(self.grid)
 
 
 
 
 if __name__ == '__main__':
-    print("Less Text!")
+    pass
