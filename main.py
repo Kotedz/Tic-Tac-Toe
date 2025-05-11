4f86ceb6cf81a3b6fe3c7132e32d4f3bf26e5ef5
Kotedz <kotedz.is.cool@gmail.com>
2025-05-11 19:08:19 +0700

    19:08

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys, random
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QMessageBox
 
+# field = board
 class Game(QWidget): # Tic-Tac-Toe
 
     def __init__(self):
         super().__init__() # parent's init
+        self.field = None
+        self.buttons = None
         self.grid = None
         self.init_ui()
 
@@ -12,6 +15,14 @@ class Game(QWidget): # Tic-Tac-Toe
         self.setWindowTitle("Кресты и Нули")
         self.setGeometry(100,100,300,300) # pos and size of window
         self.grid = QGridLayout()
+        self.buttons = [[QPushButton("") for _ in range(3)] for _ in range(3)]
+        self.field = [[None for _ in range(3)] for _ in range(3)]
+
+        for row in self.buttons:
+            for button in row:
+                button.setFixedSize(100, 100)
+                self.grid.addWidget(button, self.buttons.index(row), row.index(button))
+
         self.setLayout(self.grid)
 
 
